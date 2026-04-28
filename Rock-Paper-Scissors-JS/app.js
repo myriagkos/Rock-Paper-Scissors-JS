76ac2100d73e95d1fe4b8e0d45651ea5f5eec199
myriagkos <filipposmyriagkos@gmail.com>
2026-04-28 08:48:40 +0300

    Add files via upload

---
+++ Rock-Paper-Scissors-JS/app.js
@@ -0,0 +1,124 @@
+//Complete logic of the game inside this function
+const game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+    let moves= 0;
+
+    //Function to
+    const playGame = () => {
+        const rockBtn = document.querySelector('.rock');
+        const paperBtn = document.querySelector('.paper');
+        const scissorsBtn = document.querySelector('.scissors');
+        const playerOptions = [rockBtn, paperBtn, scissorsBtn]
+        const computerOptions = ['rock','paper', 'scissors']
+
+        //Function to start playin the game
+        playerOptions.forEach(option => {
+            option.addEventListener('click', function() {
+
+                const movesLeft = document.querySelector('.movesleft');
+                moves++;
+                movesLeft.innerText = `Moves Left: ${10- moves}`;
+
+
+                const choiceNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[choiceNumber];
+
+                //function to check who wins 
+                winner(this.innerText, computerChoice);
+
+                //calling gameOver function after 10 moves
+                if (moves == 10 ) {
+                    gameOver(playerOptions, movesLeft);
+                }
+            })
+        })
+    }
+
+    //Function to decide winner
+    const winner = (player, computer ) => {
+        const result = document.querySelector('.result');
+        const playerScoreBoard = document.querySelector('.p-count');
+        const computerScoreBoard = document.querySelector('.c-count');
+        player = player.toLowerCase();
+        computer = computer.toLowerCase();
+        if (player === computer ){
+            result.textContent = 'Tie';
+        }
+        else if (player == 'rock') {
+            if (computer == 'paper'){
+                result.textContent = 'Computer Won' ; 
+                computerScore++;
+                computerScoreBoard.textContent = computerScore;
+            }
+            else{
+                result.textContent = 'Player Won';
+                playerScore++;
+                playerScoreBoard.textContent = playerScore;
+            }
+        }
+        else if (player == 'scissors') {
+            if (computer == 'rock'){
+                result.textContent = 'Computer Won' ; 
+                computerScore++;
+                computerScoreBoard.textContent = computerScore;
+            }
+            else{
+                result.textContent = 'Player Won';
+                playerScore++;
+                playerScoreBoard.textContent = playerScore;
+            }
+        }
+        else if (player == 'paper') {
+            if (computer == 'scissors'){
+                result.textContent = 'Computer Won' ; 
+                computerScore++;
+                computerScoreBoard.textContent = computerScore;
+            }
+            else{
+                result.textContent = 'Player Won';
+                playerScore++;
+                playerScoreBoard.textContent = playerScore;
+            }  
+        }          
+    }
+
+    //function to run when game is over
+    const gameOver = (playerOptions, movesLeft) => {
+        const chooseMove = document.querySelector('.move');
+        const result = document.querySelector('.result');
+        const reloadBtn = document.querySelector('.reload');
+
+        playerOptions.forEach(option => {
+            option.style.display = 'none';
+        })
+
+        chooseMove.innerText = 'Game Over!!'
+        movesLeft.style.display = 'none';
+         
+        if (playerScore > computerScore){
+            result.style.fontSize = '2rem';
+            result.innerText= 'You Won The Game';
+            result.style.color = '#F3E3D0';
+        } else if (playerScore < computerScore){
+            result.style.fontSize = '2rem';
+            result.innerText= 'You Lost The Game';
+            result.style.color = '#D25353';
+        } else{
+            result.style.fontSize = '2rem';
+            result.innerText= 'Tie!';
+            result.style.color = '#F3E3D0';
+        }
+        reloadBtn.innerText = 'Restart';
+        reloadBtn.style.display= 'flex';
+        reloadBtn.addEventListener('click',() => {
+            window.location.reload();
+        })
+    }   
+    //calling playGame function inside game
+    playGame();
+
+}
+
+//calling game function
+game();
